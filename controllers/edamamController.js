c1a64de3ac0380c3697d510bba029d25b50080c9
cbelle <christabelle.chien@gmail.com>
2018-08-05 09:39:22 -0400

    updates

---
+++ controllers/edamamController.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const router = require("express").Router();
+const db = require("../models");
+
+const edamamFunctions = {
+
+// E D A M A M - R E L A T E D 
+  // finds recipes from db where liked = true and origin = edamam
+  findLikedEdamam: function (req, res) {
+    db.Recipe
+      .find({ 'liked': true, 'origin': 'Edamam' }
+      // , { '_id': 0, 'description': 1 }
+    )
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findByEdamam: function (req, res) {
+    db.Recipe
+      .find({ 'name': req.params.name })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  removeEdamam: function (req, res) {
+    db.Recipe
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+}
+
+router.get("/api/recipes/search/edamam/liked", edamamFunctions.findLikedEdamam)
+
+router.get("/api/recipes/search/edamam/:name", edamamFunctions.findByEdamam)
+
+router.delete("/api/recipes/edamam/:id", edamamFunctions.removeEdamam)
