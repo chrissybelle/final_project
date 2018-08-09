56077048ac26888b31669d1fbdad1c3c7b4d9fec
Gabi <gabi.cassarino@gmail.com>
2018-08-09 10:08:49 -0400

    noMatch

--- client/src/pages/NoMatch/NoMatch.js
+++ client/src/pages/NoMatch/NoMatch.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Col, Row, Container } from "../../components/Grid";
 import Jumbotron from "../../components/Jumbotron";
+import "./noMatch.css";
+
 
 class NoMatch extends React.Component {
 
@@ -9,16 +11,18 @@ class NoMatch extends React.Component {
     return (
       <Container fluid>
         <Row>
-          <Col size="md-12">
-            <Jumbotron>
-              <h1>404 Page Not Found</h1>
-              <h1>
-                <span role="img" aria-label="Face With Rolling Eyes Emoji">
-                  🙄
+          <div className="bgImage">
+            <Col size="md-12">
+              <div className="text">
+                <h1>404 Page Not Found</h1>
+                <h1>
+                  <span role="img" aria-label="Face With Rolling Eyes Emoji">
+                    🙄
                 </span>
-              </h1>
-            </Jumbotron>
-          </Col>
+                </h1>
+              </div>
+            </Col>
+          </div>
         </Row>
       </Container>
     )
